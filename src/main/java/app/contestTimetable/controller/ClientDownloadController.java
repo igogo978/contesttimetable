be5c0c84a0db961bbdddaa65e07a9e53986713bb
igogo978 <chia0210@gmail.com>
2019-12-09 16:13:46 +0800

    client page

---
+++ src/main/java/app/contestTimetable/controller/ClientDownloadController.java
@@ -0,0 +1,36 @@
+package app.contestTimetable.controller;
+
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Controller
+public class ClientDownloadController {
+
+    //user upload page
+    @GetMapping("/job/client/download")
+    public ResponseEntity<Resource> download(String param) throws IOException {
+        File file = new File("/tmp/contest/client.zip");
+
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=client.zip")
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
+
+    }
+
+}
--- src/main/java/app/contestTimetable/controller/JobController.java
+++ src/main/java/app/contestTimetable/controller/JobController.java
@@ -1,19 +1,38 @@
 package app.contestTimetable.controller;
 
-import app.contestTimetable.model.Job;
 import app.contestTimetable.repository.ReportRepository;
 import app.contestTimetable.service.JobService;
+import app.contestTimetable.storage.StorageProperties;
+import app.contestTimetable.storage.StorageService;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 public class JobController {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    StorageProperties storageProperties;
+
+    private final StorageService storageService;
+
+    @Autowired
+    public JobController(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
 
     @Autowired
@@ -23,10 +42,28 @@ public class JobController {
     ReportRepository reportrepository;
 
     @GetMapping(value = "/job/run")
-    public String runReport() {
+    public String runReport(Model model) {
+
+        model.addAttribute("client", new File("/tmp/contest/client.zip").exists());
 
         return "jobrun";
 
+    }
+
+
+    //user upload page
+    @PostMapping("/job/client/upload")
+    public String handleReportFileUpload(@RequestParam("file") MultipartFile file,
+                                         RedirectAttributes redirectAttributes) throws IOException {
+//        logger.info("filename:" + file.getOriginalFilename());
+        logger.info("filename:" + "client.zip");
+        String filename = file.getOriginalFilename();
+        storageService.store(file);
+        //move to /tmp/contest/
+        FileUtils.forceMkdir(new File("/tmp/contest"));
+
+        FileUtils.copyFile(new File("/tmp/client.zip"), new File("/tmp/contest/client.zip"));
+        return "redirect:/job/run";
     }
 
 
