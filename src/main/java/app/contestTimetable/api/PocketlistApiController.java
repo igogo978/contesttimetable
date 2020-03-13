8bbdc9879f833c1d48bb292544b4abd9605f957a
igogo978 <chia0210@gmail.com>
2020-03-13 16:29:43 +0800

    inform team,cover

--- src/main/java/app/contestTimetable/api/PocketlistApiController.java
+++ src/main/java/app/contestTimetable/api/PocketlistApiController.java
@@ -43,6 +43,7 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @RestController
 public class PocketlistApiController {
@@ -171,16 +172,123 @@ public class PocketlistApiController {
 
     }
 
+    @GetMapping(value = "/api/pocketlist/inform/team/download")
+    public ResponseEntity<Resource> doInformTeam() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-    @GetMapping(value = "/api/pocketlist/inform/cover/download")
-    public ResponseEntity<Resource> downloadPocketlistReportInformTeacher() throws IOException {
-        String filename = "inform-teacher";
-        logger.info("create pdf");
+        baos = doPDFTeam();
+        logger.info("pdf file has been created ");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("charset", "utf-8");
+        headers.setContentDispositionFormData("attachment", String.format("%s", "inform.pdf"));
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        Resource resource = new InputStreamResource(new ByteArrayInputStream(baos.toByteArray()));
+        return ResponseEntity.ok().headers(headers).body(resource);
+
+    }
+
+
+    ByteArrayOutputStream doPDFTeam() throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(baos));
         Document document = new Document(pdfDoc);
+
+
         // Create a PdfFont
-        PdfFont font = PdfFontFactory.createFont("/home/igogo/font/TW-Kai-98_1.ttf", PdfEncodings.IDENTITY_H, true);
+        PdfFont font = PdfFontFactory.createFont("/opt/font/TW-Kai-98_1.ttf", PdfEncodings.IDENTITY_H, true);
+
+
+        for (int i = 0; i < 2; i++) {
+
+            if (i != 0) {
+                document.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
+            }
+
+            Paragraph head = new Paragraph();
+            head.add("臺中市108年度中小學資訊網路應用競賽決賽選手帳號密碼通知單").setFont(font).setBold().setFontSize(29).setTextAlignment(TextAlignment.CENTER);
+
+
+            document.add(head);
+
+            Paragraph blank = new Paragraph("\n");
+            document.add(blank);
+
+
+            Table table = new Table(UnitValue.createPercentArray(1)).useAllAvailableWidth();
+
+            Paragraph paragraph = new Paragraph(String.format("試場學校： %s", "大甲區華龍國民小學")).setFont(font);
+            paragraph.add("\n");
+            paragraph.add(String.format("競賽日期：%s", "3/12(二)上午9:00-12:00"));
+            paragraph.add("\n");
+            paragraph.add(String.format("競賽項目：%s","SCRATCH應用競賽國中甲組程式設計組"));
+            Cell cell = new Cell()
+                    .setTextAlignment(TextAlignment.LEFT)
+                    .setFont(font)
+                    .setFontSize(20)
+                    .add(blank)
+                    .add(paragraph)
+                    .add(blank);
+            table.addCell(cell);
+
+            //---------------------------------------------------------------------------------
+
+            paragraph = new Paragraph(String.format("學校：%s", "大甲區大甲國中")).setFont(font);
+            paragraph.add("\n");
+            paragraph.add(String.format("姓名：%s ", "郭碧崑"));
+
+
+            cell = new Cell()
+                    .setTextAlignment(TextAlignment.LEFT)
+                    .setFont(font)
+                    .setFontSize(20)
+                    .add(blank)
+                    .add(paragraph)
+                    .add(blank);
+            table.addCell(cell);
+
+
+            paragraph = new Paragraph(String.format("決賽注意事項")).setFont(font).setTextAlignment(TextAlignment.CENTER).setBold().setUnderline();
+            paragraph.add("\n");
+//            paragraph.add(String.format("1、「所有競賽項目」的題目於競賽時間開始時自動轉址公布，請記得重新整理網頁，就可看到題目；「所有競賽項目」的作品內容都不得出現姓名及學校等相關資訊。"));
+            String text = "1、「所有競賽項目」的題目於競賽時間開始時自動轉址公布，請記得重新整理網頁，就可看到題目；「所有競賽項目」的作品內容都不得出現姓名及學校等相關資訊。\n" +
+                    "2、競賽時間開始後，遲到30分鐘以上不得入場。競賽開始超過40分鐘後，參賽學生始得離開試場。\n" +
+                    "3、競賽時間內不得自行攜帶使用任何形式之可攜式儲存媒體及通訊設備，一經發現立即取消參賽資格。（競賽主辦單位借給每位參賽者空白隨身碟一支，供暫存使用）。\n" +
+                    "4、競賽時間場地僅能連線至資訊網路應用競賽系統，不提供其他對外之網路連線。競賽時間終了即無法再上傳，參賽者請及早上傳並自行注意時間掌控，避免網路壅塞影響上傳。";
+            Paragraph paragraph2 = new Paragraph(text).setTextAlignment(TextAlignment.LEFT);
+
+            cell = new Cell()
+//                    .setTextAlignment(TextAlignment.LEFT)
+                    .setFont(font)
+                    .setFontSize(16)
+//                    .add(blank)
+                    .add(paragraph)
+                    .add(paragraph2)
+                    .add(blank);
+            table.addCell(cell);
+
+
+            document.add(table);
+
+        }
+
+
+        document.close();
+
+
+        return baos;
+
+    }
+
+    @GetMapping(value = "/api/pocketlist/inform/cover/download")
+    public ResponseEntity<Resource> doInformCover() throws IOException {
+
+
+        // Create a PdfFont
+        PdfFont font = PdfFontFactory.createFont("/opt/font/TW-Kai-98_1.ttf", PdfEncodings.IDENTITY_H, true);
 
 //        contestconfigRepository.findAll().forEach(contestconfig -> logger.info(String.valueOf(contestconfig.getId())+contestconfig.getDescription()));
         List<Contestconfig> configs = contestconfigRepository.findAllByOrderByIdAsc();
@@ -192,43 +300,79 @@ public class PocketlistApiController {
 
 //        locations.forEach(location -> logger.info(location.getLocationname()));
         List<Informcover> informs = new ArrayList<>();
+
+        //4 个场次
         configs.forEach(config -> {
 //            logger.info(String.format("%s,%s,%s", config.getId(), config.getContestgroup(), config.getDescription()));
 
 
-            //4 个场次
-            config.getContestgroup().forEach(contestitem -> {
-//                List<Team> teams = teamRepository.findByContestitemContainingOrderByLocationDesc(contestitem.toUpperCase());
+            locations.forEach(location -> {
+                Informcover inform = new Informcover();
 
-                locations.forEach(location -> {
-                    Informcover inform = new Informcover();
+                inform.setContestItem(String.join("、", config.getContestgroup()).toUpperCase());
+                inform.setTeams(0);
+                inform.setLocation(location.getLocationname());
+                inform.setDescription(config.getDescription());
+                AtomicReference<Integer> teamsize = new AtomicReference<>(0);
+                AtomicReference<Integer> totalpeople = new AtomicReference<>(0);
 
-                    inform.setLocation(location.getLocationname());
-                    inform.setContestItem(contestitem.toUpperCase());
+                config.getContestgroup().forEach(contestitem -> {
                     List<Team> teams = teamRepository.findByLocationAndContestitemContainingOrderByLocationDesc(location.getLocationname(), contestitem.toUpperCase());
-                    inform.setTeams(teams.size());
-//                    teams.forEach(team -> {
-//                        logger.info(String.format("%s,%s at %s", contestitem, team.getContestitem(), team.getLocation()));
-//
-//                    });
-
-                    logger.info(String.format("%s,%s,%s", inform.getLocation(), inform.getContestItem(), inform.getTeams()));
+                    teams.forEach(team -> {
+                        if (team.getMembername() != null) {
+//                            logger.info(String.format("%s,%s", team.getUsername(), team.getMembername()));
+                            totalpeople.updateAndGet(v -> v + 2);
+                        } else {
+                            totalpeople.updateAndGet(v -> v + 1);
+                        }
+                    });
+                    teamsize.updateAndGet(v -> v + teams.size());
                 });
-
-
+                inform.setTeams(teamsize.get());
+                inform.setTotalPeople(totalpeople.get());
+                informs.add(inform);
             });
 
         });
 
 
-        for (int i = 0; i < 2; i++) {
+        String filename = "inform-cover";
+        logger.info("create pdf");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+
+        baos = doPDFCover(informs);
+        logger.info("pdf file has been created ");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        headers.add("charset", "utf-8");
+        headers.setContentDispositionFormData("attachment", String.format("%s", filename));
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        Resource resource = new InputStreamResource(new ByteArrayInputStream(baos.toByteArray()));
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
+    ByteArrayOutputStream doPDFCover(List<Informcover> informs) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(baos));
+        Document document = new Document(pdfDoc);
+
+
+        // Create a PdfFont
+        PdfFont font = PdfFontFactory.createFont("/opt/font/TW-Kai-98_1.ttf", PdfEncodings.IDENTITY_H, true);
+
+
+        for (int i = 0; i < informs.size(); i++) {
 
             if (i != 0) {
                 document.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
             }
 
             Paragraph head = new Paragraph();
-            head.add("臺中市108年度中小學資訊網路應用競賽決賽選手帳號密碼").setFont(font).setBold().setFontSize(40).setTextAlignment(TextAlignment.CENTER);
+            head.add("臺中市108年度中小學資訊網路應用競賽決賽選手帳號密碼").setFont(font).setBold().setFontSize(29).setTextAlignment(TextAlignment.CENTER);
 
 
             document.add(head);
@@ -239,9 +383,9 @@ public class PocketlistApiController {
 
             Table table = new Table(UnitValue.createPercentArray(1)).useAllAvailableWidth();
 
-            Paragraph paragraph = new Paragraph("試場學校：西區大同國民小學").setFont(font);
+            Paragraph paragraph = new Paragraph(String.format("試場學校： %s", informs.get(i).getLocation())).setFont(font);
             paragraph.add("\n");
-            paragraph.add("競賽時間：3/12(二)上午9:00-12:00");
+            paragraph.add(String.format("競賽時間：%s", informs.get(i).getDescription()));
             Cell cell = new Cell()
                     .setTextAlignment(TextAlignment.CENTER)
                     .setFont(font)
@@ -253,7 +397,7 @@ public class PocketlistApiController {
 
             paragraph = new Paragraph("項目類別").setFont(font);
             paragraph.add("\n");
-            paragraph.add("SCRATCH應用競賽(全部)");
+            paragraph.add(String.format("%s", informs.get(i).getContestItem()));
 
 
             cell = new Cell()
@@ -266,9 +410,9 @@ public class PocketlistApiController {
             table.addCell(cell);
 
 
-            paragraph = new Paragraph("決賽選手數量：26人").setFont(font);
+            paragraph = new Paragraph(String.format("決賽選手數量：%s 人", informs.get(i).getTotalPeople())).setFont(font);
             paragraph.add("\n");
-            paragraph.add("帳號密碼通知單數量：26張");
+            paragraph.add(String.format("帳號密碼通知單數量：%s 張", informs.get(i).getTeams()));
 
 
             cell = new Cell()
@@ -287,17 +431,9 @@ public class PocketlistApiController {
 
 
         document.close();
-        logger.info("pdf file has been created ");
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Pragma", "no-cache");
-        headers.add("Expires", "0");
-        headers.add("charset", "utf-8");
-        headers.setContentDispositionFormData("attachment", String.format("%s", "inform.pdf"));
-        headers.setContentType(MediaType.APPLICATION_PDF);
-        Resource resource = new InputStreamResource(new ByteArrayInputStream(baos.toByteArray()));
-        return ResponseEntity.ok().headers(headers).body(resource);
+
+        return baos;
     }
 
 
